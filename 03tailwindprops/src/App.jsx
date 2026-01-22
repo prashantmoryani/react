2cfe09cb3e8a86dc0aaa0639387fca877273c829
prashantmoryani <prashantmoryani@gmail.com>
2026-01-22 15:28:01 +0530

    Add 03tailwind props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Card from './components/Card.jsx'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-green-400 text-black'>Tailwind Test</h1>
+      <Card username = "Prashant" Price = "100"/>
+      <Card username = "Moryani"/>
+    </>
+  )
+}
+
+export default App
